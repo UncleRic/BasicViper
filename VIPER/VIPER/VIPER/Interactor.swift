701ef354e7058aa6dc7ae23a98e4fd3aea7c68c7
Frederick C. Lee <frederick.c.lee@gmail.com>
2022-01-13 13:18:38 -0800

    Added CapitalOne Data Skeleton

--- VIPER/VIPER/VIPER/Interactor.swift
+++ VIPER/VIPER/VIPER/Interactor.swift
@@ -8,6 +8,8 @@
 //  Purpose to get/post data to either database or network
 //
 //  Demo API endpoint: https://jsonplaceholder.typicode.com/users
+//  Capital-One Demo endpoint: https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key=%20MCKwfxg3mMkqmjdfdzCZYeG4GqcMGvZ3
+//
 //  Created by Frederick Lee on 1/12/22.
 //
 
